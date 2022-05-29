c5c36a61a055ba9b2b3971a7ffde40b3f4385d0b
JK-8989 <jiyoungkim365@gmail.com>
2022-05-29 14:38:59 -0700

    Add full image div + js-function

--- script.js
+++ script.js
@@ -3,8 +3,20 @@ lightboxBg.id = 'lightbox-bg';
 document.body.appendChild(lightboxBg);
 
 const images = document.querySelectorAll('img');
+
 images.forEach(images => {
   images.addEventListener('click', ()=>{
+
     lightboxBg.classList.add('active');
+    const fullImage = document.createElement('img');
+    fullImage.src = images.src;
+    fullImage.id = 'full-image';  
+
+    lightboxBg.appendChild(fullImage);
+       
   })
+})
+
+lightboxBg.addEventListener('click', () => {
+  lightboxBg.classList.remove('active');
 })
